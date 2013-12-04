3e7c24574c4bba60a8c2f7414ae4084350fdc744
evanwong1990 <evanwong1990@2254c799-db73-c7b3-05fb-ec018eb743b8>
2013-12-04 10:25:38 +0000

    commit deposit

--- trunk/DCS/src/com/bank/gui/DepositPanel.java
+++ trunk/DCS/src/com/bank/gui/DepositPanel.java
@@ -1,10 +1,24 @@
 package com.bank.gui;
 
+import com.bank.entity.Branch;
+import com.bank.entity.MySQLConnection;
+import com.bank.utils.CommunicationWrapper;
+import com.bank.utils.Operation;
+import com.bank.utils.TextFieldLimiter;
+import com.bank.utils.Toast;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.text.AbstractDocument;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Moofie
@@ -14,8 +28,25 @@ public class DepositPanel extends javax.swing.JPanel {
     /**
      * Creates new form DepositPanel
      */
+    private boolean validate;
+
     public DepositPanel() {
         initComponents();
+    }
+
+    private void reset() {
+        txtAccNumber.setText("");
+        txtICNumber.setText("");
+        txtAmountDeposit.setText("");
+    }
+
+    public static boolean isNumeric(String str) {
+        try {
+            double d = Double.parseDouble(str);
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+        return true;
     }
 
     /**
@@ -28,61 +59,149 @@ public class DepositPanel extends javax.swing.JPanel {
     private void initComponents() {
 
         jLabel1 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
+        txtAccNumber = new javax.swing.JTextField();
         jLabel2 = new javax.swing.JLabel();
-        jTextField2 = new javax.swing.JTextField();
+        txtICNumber = new javax.swing.JTextField();
         jLabel3 = new javax.swing.JLabel();
-        jTextField3 = new javax.swing.JTextField();
-        jButton2 = new javax.swing.JButton();
-        jButton1 = new javax.swing.JButton();
+        txtAmountDeposit = new javax.swing.JTextField();
+        btnReset = new javax.swing.JButton();
+        btnSubmit = new javax.swing.JButton();
 
         setLayout(null);
 
         jLabel1.setText("Account Number ");
         add(jLabel1);
         jLabel1.setBounds(20, 60, 220, 30);
-        add(jTextField1);
-        jTextField1.setBounds(20, 90, 390, 30);
+
+        AbstractDocument aDoc = (AbstractDocument)txtAccNumber.getDocument();
+        aDoc.setDocumentFilter(new TextFieldLimiter("\\d{0,14}"));
+        add(txtAccNumber);
+        txtAccNumber.setBounds(20, 90, 390, 30);
 
         jLabel2.setText("IC Number");
         add(jLabel2);
         jLabel2.setBounds(20, 130, 220, 30);
-        add(jTextField2);
-        jTextField2.setBounds(20, 160, 390, 30);
+
+        AbstractDocument aDocIC = (AbstractDocument)txtICNumber.getDocument();
+        aDocIC.setDocumentFilter(new TextFieldLimiter("\\d{0,12}"));
+        add(txtICNumber);
+        txtICNumber.setBounds(20, 160, 390, 30);
 
         jLabel3.setText("Amount to Deposit");
         add(jLabel3);
         jLabel3.setBounds(20, 200, 180, 30);
 
-        jTextField3.addActionListener(new java.awt.event.ActionListener() {
+        AbstractDocument aDocAmount = (AbstractDocument)txtAmountDeposit.getDocument();
+        aDocAmount.setDocumentFilter(new TextFieldLimiter("\\d{0,5}"));
+        txtAmountDeposit.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextField3ActionPerformed(evt);
+                txtAmountDepositActionPerformed(evt);
             }
         });
-        add(jTextField3);
-        jTextField3.setBounds(20, 230, 390, 30);
+        add(txtAmountDeposit);
+        txtAmountDeposit.setBounds(20, 230, 390, 30);
 
-        jButton2.setText("Reset");
-        add(jButton2);
-        jButton2.setBounds(10, 320, 170, 50);
+        btnReset.setText("Reset");
+        btnReset.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnResetActionPerformed(evt);
+            }
+        });
+        add(btnReset);
+        btnReset.setBounds(10, 320, 170, 50);
 
-        jButton1.setText("Submit");
-        add(jButton1);
-        jButton1.setBounds(260, 320, 170, 50);
+        btnSubmit.setText("Submit");
+        btnSubmit.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnSubmitActionPerformed(evt);
+            }
+        });
+        add(btnSubmit);
+        btnSubmit.setBounds(260, 320, 170, 50);
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jTextField3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField3ActionPerformed
+    private void txtAmountDepositActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtAmountDepositActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jTextField3ActionPerformed
+    }//GEN-LAST:event_txtAmountDepositActionPerformed
 
+    private void btnResetActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnResetActionPerformed
+        reset();
+    }//GEN-LAST:event_btnResetActionPerformed
+
+    private void btnSubmitActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSubmitActionPerformed
+        StringBuilder check = new StringBuilder("<html>Errors");
+
+        validate = true;
+
+
+
+        if (txtAccNumber.getText().length() == 0) {
+            validate = false;
+            check.append("<br />Please enter account number");
+        } 
+
+        if (txtICNumber.getText().length() == 0) {
+            validate = false;
+            check.append("<br />Please enter ic number");
+        }
+
+        if (txtAmountDeposit.getText().length() == 0) {
+            validate = false;
+            check.append("<br />Please enter deposit amount");
+        } 
+
+        if (!validate) {
+            check.append("</html>");
+            Toast.makeText(getParent(), 150, "" + check, Toast.LENGTH_LONG).display();
+        } else {
+            CommunicationWrapper cw = null;
+            try {
+                cw = new CommunicationWrapper(5500);
+            } catch (SocketException ex) {
+                Logger.getLogger(DepositPanel.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+
+            if (cw.isBranchReachable("6652", "6649")) {
+            try {
+
+                    JSONObject j = new JSONObject();
+                    j.put("operation", Operation.DEPOSIT);
+                    String message;
+                    JSONObject content = new JSONObject();
+                    content.put("accNo", txtAccNumber.getText());
+                    content.put("icNo", txtICNumber.getText());
+                    content.put("amount", Double.parseDouble(txtAmountDeposit.getText()));
+                    content.put("port", 5500);
+                    content.put("address", InetAddress.getLocalHost().getHostAddress());
+                    System.out.println(InetAddress.getLocalHost().getHostAddress());
+                    j.put("content", content);
+                    Branch b = new Branch();
+                    b.setBranchCode("6649");
+                    String ip = b.obtainBranchIp(MySQLConnection.getConnection());
+                    cw.send(j, InetAddress.getByName(ip), 5000);
+                    JSONObject js = cw.receive();
+                    System.out.println(js.toString());
+                } catch (JSONException ex) {
+                    ex.printStackTrace();
+                System.out.println(ex);
+            } catch (UnknownHostException ex) {
+                ex.printStackTrace();
+                Logger.getLogger(DepositPanel.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            } else {
+                System.out.println("Branch unreacbable");
+    }
+        }
+    }//GEN-LAST:event_btnSubmitActionPerformed
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
+    private javax.swing.JButton btnReset;
+    private javax.swing.JButton btnSubmit;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
-    private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField2;
-    private javax.swing.JTextField jTextField3;
+    private javax.swing.JTextField txtAccNumber;
+    private javax.swing.JTextField txtAmountDeposit;
+    private javax.swing.JTextField txtICNumber;
     // End of variables declaration//GEN-END:variables
 }
